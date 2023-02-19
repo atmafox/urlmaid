ffd03a91a0bc719d36abeeeb439e2f59ae093977
Atma Yhhsom'aa <adrutledge@gmail.com>
2023-02-18 22:30:04 -0700

    FEATURE: Refactoring and adding support for Accept header to vary output

---
+++ api.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type URLToEncode struct {
+	URL  string `json:"url"`
+	Type string `json:"type"`
+}
+
+type postsResource struct{}
+
+// Let's set up our router for the API
+func (rs postsResource) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	r.Get("/v1/supported", rs.Supp)
+	r.Post("/v1/tidy", rs.Tidy)
+
+	return r
+}
+
+// This one is unique in that it outputs plain and only uses a template to make things easier to update
+func (rs postsResource) Supp(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	a := r.Header["Accept"][0]
+
+	switch {
+	case strings.HasPrefix(a, "text/plain"):
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+		// TODO: Generate the list of supported types 'automatically'
+		fmt.Fprint(w, `ebay
+amazon`)
+		return
+	case strings.HasPrefix(a, "application/json"):
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+		s := [2]string{
+			"ebay",
+			"amazon",
+		}
+
+		e := json.NewEncoder(w)
+
+		err := e.Encode(s)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+
+		return
+	default:
+		http.Error(w, "Bad reqeust", http.StatusBadRequest)
+		return
+	}
+}
+
+func (rs postsResource) Tidy(w http.ResponseWriter, r *http.Request) {
+	dec := json.NewDecoder(r.Body)
+	enc := json.NewEncoder(w)
+
+	defer r.Body.Close()
+
+	var u URLToEncode
+	err := dec.Decode(&u)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	out := doTidy(u.Type, url.QueryEscape(u.URL), w)
+	a := r.Header["Accept"][0]
+	switch {
+	case strings.HasPrefix(a, "text/plain"):
+		fmt.Fprintf(w, "%s", out)
+
+	case strings.HasPrefix(a, "application/json"):
+		err := enc.Encode(out)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+	default:
+		http.Error(w, "Bad request", http.StatusBadRequest)
+	}
+
+}
+
+func doTidy(t string, u string, w http.ResponseWriter) string {
+	switch t {
+	case "autodetect":
+		// Figure out how to autodetect
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return ""
+	case "ebay":
+		d, err := url.QueryUnescape(u)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return ""
+		}
+
+		out := strings.Split(d, "?")[0]
+
+		return out
+	case "amazon":
+		d, err := url.QueryUnescape(u)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return ""
+		}
+
+		r, err := regexp.Compile(`(?P<useful>/dp/[[:alnum:]]+)/`)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return ""
+		}
+
+		match := r.FindStringSubmatch(d)
+		result := make(map[string]string)
+
+		for i, name := range r.SubexpNames() {
+			if i != 0 && name != "" {
+				result[name] = match[i]
+			}
+		}
+
+		out := fmt.Sprintf("https://amazon.com%s", result["useful"])
+		return out
+	case "default":
+		// TODO: Perhaps a different error code is better for an API?  Research.
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return ""
+	}
+
+	return ""
+}
--- main.go
+++ main.go
@@ -1,127 +1,38 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"net/url"
 	"path/filepath"
-	"regexp"
-	"strings"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-type CleanedURL struct {
-	Type    template.HTML
-	Input   template.HTML
-	Cleaned template.HTML
-}
-
-type URLToEncode struct {
-	URL  string
-	Type string
-}
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
 	execTemplate(w, filepath.Join("templates", "home.gohtml"))
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
 	execTemplate(w, filepath.Join("templates", "contact.gohtml"))
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
 	execTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func urlHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
 	execTemplate(w, filepath.Join("templates", "url.gohtml"))
-}
-
-// This one is unique in that it outputs plain and only uses a template to make things easier to update
-func suppHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	// TODO: Generate the list of supported types 'automatically'
-	fmt.Fprint(w, `ebay
-amazon`)
-}
-
-func tidyHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	t := chi.URLParam(r, "type")
-	u := chi.URLParam(r, "url")
-
-	// TODO: Verify sanity of request
-
-	doTidy(t, u, w)
-}
-
-func tidyPost(w http.ResponseWriter, r *http.Request) {
-	dec := json.NewDecoder(r.Body)
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	var u URLToEncode
-	err := dec.Decode(&u)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	doTidy(u.Type, url.QueryEscape(u.URL), w)
-}
-
-func doTidy(t string, u string, w http.ResponseWriter) {
-	switch t {
-	case "autodetect":
-		// Figure out how to autodetect
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	case "ebay":
-		d, err := url.QueryUnescape(u)
-		if err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-			return
-		}
-
-		out := strings.Split(d, "?")[0]
-
-		fmt.Fprintf(w, "%s", out)
-		return
-	case "amazon":
-		d, err := url.QueryUnescape(u)
-		if err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-			return
-		}
-
-		r, err := regexp.Compile(`(?P<useful>/dp/[[:alnum:]]+)/`)
-		if err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-			return
-		}
-
-		match := r.FindStringSubmatch(d)
-		result := make(map[string]string)
-
-		for i, name := range r.SubexpNames() {
-			if i != 0 && name != "" {
-				result[name] = match[i]
-			}
-		}
-
-		fmt.Fprintf(w, "https://amazon.com%s", result["useful"])
-		return
-	case "default":
-		// TODO: Perhaps a different error code is better for an API?  Research.
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
 }
 
 func execTemplate(w http.ResponseWriter, filepath string) {
@@ -145,15 +56,13 @@ func execTemplate(w http.ResponseWriter, filepath string) {
 func main() {
 	r := chi.NewRouter()
 
+	r.Use(middleware.Logger)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
 	r.Get("/url", urlHandler)
-	r.Get("/api/supported", suppHandler)
-	r.Get("/api/v1/supported", suppHandler)
-	r.Get("/api/tidy/{type}/{url}", tidyHandler)
-	r.Get("/api/v1/tidy/{type}/{url}", tidyHandler)
-	r.Post("/api/v1/tidy", tidyPost)
+	r.Mount("/api", postsResource{}.Routes())
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
