3deb49e4b6ba58bf78f7b1220ddb09bb86d847d0
Atma Yhhsom'aa <adrutledge@gmail.com>
2023-03-19 19:04:43 -0600

    FEATURE: HTTPS support via letsencrypt/autocert (#1)
    * FEATURE: Pulling in autocert and adjusting page service paths for cleanliness
    
    * FEATURE: Restructing router to handle HTTP and HTTPS as well as flags to enable/disable it
    
    * FEATURE: Finishing up HTTPS/letsencrypt support but receiving 404s for well knowns even though content is served
    
    * FEATURE: Finishing up HTTPS/autocert support

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/atmafox/urlmaid
 go 1.19
 
 require github.com/go-chi/chi/v5 v5.0.8
+
+require (
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,34 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
+	"golang.org/x/crypto/acme/autocert"
+
 	_ "github.com/atmafox/urlmaid/tidyProviders/_all"
 )
+
+var (
+	flgProduction   = false
+	flgRedirectHTTP = false
+)
+
+func parseFlags() {
+	flag.BoolVar(&flgProduction, "production", false, "If true then start HTTPS server, generating letsencrypt cert as needed")
+	flag.BoolVar(&flgRedirectHTTP, "redirect-to-https", false, "If true then redirect HTTP to HTTPS traffic")
+	flag.Parse()
+}
+
+func init() {
+	parseFlags()
+}
+
+type redirectHandler struct{}
+
+func (handler redirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	newURI := "https://" + r.Host + r.URL.String()
+	http.Redirect(w, r, newURI, http.StatusFound)
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	execTemplate(w, filepath.Join("templates", "home.gohtml"))
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-
-	execTemplate(w, filepath.Join("templates", "contact.gohtml"))
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-
-	execTemplate(w, filepath.Join("templates", "faq.gohtml"))
+	execTemplate(w, filepath.Join("templates", "tidy.gohtml"))
 }
 
 func execTemplate(w http.ResponseWriter, filepath string) {
@@ -49,16 +63,57 @@ func execTemplate(w http.ResponseWriter, filepath string) {
 	}
 }
 
-func main() {
-	r := chi.NewRouter()
-
+func configRouter(r chi.Router) {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+}
 
+func main() {
+	var httpsSrv *http.Server
+	var h *autocert.Manager
+	certFile, keyFile := "", ""
+	var rHTTP chi.Router
+	var r chi.Router
+
+	if flgProduction {
+		dataDir := "."
+
+		ha := redirectHandler{}
+
+		r = chi.NewRouter()
+
+		httpsSrv = &http.Server{
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 5 * time.Second,
+			IdleTimeout:  120 * time.Second,
+			Handler:      r,
+		}
+		h = &autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: autocert.HostWhitelist("urlmaid.ayerie.com"),
+			Cache:      autocert.DirCache(dataDir),
+		}
+		httpsSrv.Addr = ":3001"
+		httpsSrv.TLSConfig = h.TLSConfig()
+
+		rHTTP = chi.NewRouter()
+		configRouter(rHTTP)
+		rHTTP.Handle("/*", h.HTTPHandler(ha))
+
+		go func() {
+			err := http.ListenAndServe(":3000", rHTTP)
+			if err != nil {
+				log.Fatalf("Cannot start HTTP listener: %s\n", err)
+			}
+		}()
+	} else {
+		r = chi.NewRouter()
+	}
+
+	configRouter(r)
 	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
 	fs := http.FileServer(http.Dir("static"))
 	r.Handle("/static/*", http.StripPrefix("/static/", fs))
 	r.Mount("/api", postsResource{}.Routes())
@@ -66,8 +121,16 @@ func main() {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
-	//tidyProviders.InitTidyProviders()
-
-	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", r)
+	fmt.Println("Starting the server...")
+	if flgProduction {
+		err := httpsSrv.ListenAndServeTLS(certFile, keyFile)
+		if err != nil {
+			log.Fatalf("Listening on tcp/3001 for HTTPS failed: %s\n", err)
+		}
+	} else {
+		err := http.ListenAndServe(":3000", r)
+		if err != nil {
+			log.Fatalf("Listening on tcp/3000 for HTTP failed: %s\n", err)
+		}
+	}
 }
